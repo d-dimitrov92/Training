7a175fbd536f22a95a233ecbb5f1bed3c7c2f016
d-dimitrov92 <eng.d.dimitrov92@gmail.com>
2018-04-18 16:55:49 +0300

    changes on wor

---
+++ C# Fundamentals/04.ConsoleInOut/04.ConsoleInOut/07.SumOfFiveIntegersWithErrorMessage/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.SumOfFiveIntegersWithErrorMessage
+{
+    class Startup
+    {
+        static void Main()
+        {
+            int a;
+            string errorMessage = "Enter an integer";
+            Console.Write("Enter five numbers. After every number hit Enter button: \n");
+            bool firstBool = int.TryParse(Console.ReadLine(), out a);
+
+            
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+            int fourth = int.Parse(Console.ReadLine());
+            int fifth = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
