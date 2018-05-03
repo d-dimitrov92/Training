40aa38a0d910d58e24d569121511453d8969b9b3
d-dimitrov92 <eng.d.dimitrov92@gmail.com>
2018-05-03 23:39:38 +0300

    home

---
+++ C# Fundamentals/06.Loops/06.Factoriel/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _06.Factoriel
+{
+    class Startup
+    {
+        static void Main()
+        {
+            Console.Write("N = ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("K = ");
+            int k = int.Parse(Console.ReadLine());
+
+            int nFact = 1;
+            int kFact = 1;
+
+            if (1 < k && k < n)
+            {
+                do
+                {
+                    nFact *= n;
+                    n--;
+                }
+                while (n > 0);
+
+                do
+                {
+                    kFact *= k;
+                    k--;
+                }
+                while (k > 0);
+
+                Console.WriteLine("\nResult = " + (nFact / kFact) + "\n");
+            }
+
+            else
+            {
+                Console.WriteLine("\nError!\n\n");
+            }
+        }
+    }
+}
