0e568957c637b364ddc970c1c1c467927169e85b
d-dimitrov92 <eng.d.dimitrov92@gmail.com>
2018-05-10 00:11:53 +0300

    work

---
+++ C# Fundamentals/101.Exams/2011-2012 Part 1 - Sample Exam/03.ForestRoad/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _03.ForestRoad
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            byte n = byte.Parse(Console.ReadLine());
+            for (int path = 0; path < n; path++)
+            {
+                Console.Write(new string('.', path));
+                Console.Write('*');
+                Console.Write(new string('.', (n - 1) - path));
+                Console.WriteLine();
+            }
+            for (int path = n - 1; path > 0; path--)
+            {
+                Console.Write(new string('.', path - 1));
+                Console.Write('*');
+                Console.Write(new string('.', n - path));
+                Console.WriteLine();
+            }
+        }
+    }
+}
