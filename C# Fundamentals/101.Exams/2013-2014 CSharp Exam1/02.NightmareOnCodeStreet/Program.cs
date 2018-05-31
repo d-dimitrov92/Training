44286b5b2873af5f0b833cce55f881bac795b482
d-dimitrov92 <eng.d.dimitrov92@gmail.com>
2018-06-01 00:40:25 +0300

    home

---
+++ C# Fundamentals/101.Exams/2013-2014 CSharp Exam1/02.NightmareOnCodeStreet/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02.NightmareOnCodeStreet
+{
+    class Program
+    {
+        static void Main()
+        {
+            string str = Console.ReadLine();
+            int oddPos = str.Length / 2;
+            int res = 0;
+            int count = 0;
+            
+            for (int i=1; i<str.Length; i = i + 2)
+            {
+                if (char.IsDigit(str[i]))
+                {
+                    res += str[i] - '0';
+                    count++;
+                }
+            }
+
+            Console.WriteLine("{0} {1}", count, res);
+        }
+    }
+}
