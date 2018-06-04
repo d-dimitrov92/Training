9e73b7beb65c624cf35adb3475c68ef3d3ca0cae
d-dimitrov92 <eng.d.dimitrov92@gmail.com>
2018-06-04 13:12:28 +0300

    work

---
+++ C# Fundamentals/06.Loops/06.Factorial/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _06.Factorial
+{
+    class Startup
+    {
+        static void Main()
+        {
+            decimal nFact = 1;
+            decimal kFact = 1;
+
+            Console.Write("Enter N = ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter K = ");
+            int k = int.Parse(Console.ReadLine());
+
+            if (1 < k && k < n)
+            {
+                do
+                {
+                    nFact *= n;
+                    n--;
+                }
+                while (n > 0);
+
+                do
+                {
+                    kFact *= k;
+                    k--;
+                }
+                while (k > 0);
+
+                Console.WriteLine(nFact / kFact);
+            }
+            else
+            {
+                Console.WriteLine(" 1 < K < N ");
+            }
+        }
+    }
+}
